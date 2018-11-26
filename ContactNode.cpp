81fe4257a2c256c4d65193616167830dd7085517
Sally Shi <sally.l.shi@gmail.com>
2018-11-25 19:49:13 -0800

    Add files via upload

---
+++ ContactNode.cpp
@@ -0,0 +1,8 @@
+#include "ContactNode.h"
+
+    ContactNode(string name, string phone);
+    void InsertAfter();
+    string GetName();
+    string GetPhoneNumber();
+    ContactNode GetNext();
+    void PrintContactNode();
